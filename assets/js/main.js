07e00cb674d06c74eaca1fd60b934f597608e7b7
Kelsey Z <kelsey@Kelseys-MacBook-Pro.local>
2017-02-09 15:01:47 -0500

    motion

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,4 +27,53 @@ $(document).ready(function() {
 		console.log('fuck');
 	});
 
+	$('.circle').hover(function(){
+		$('.boom-5').removeClass('blink-5');
+		$('.boom-5').addClass('blink-5');
+		console.log('fuck');
+	});
+
+	$('.circle').hover(function(){
+		$('.boom-6').removeClass('blink-6');
+		$('.boom-6').addClass('blink-6');
+		console.log('fuck');
+	});
+
+	$('.circle').hover(function(){
+		$('.boom-7').removeClass('blink-7');
+		$('.boom-7').addClass('blink-7');
+		console.log('fuck');
+	});
+
+	$('.circle').hover(function(){
+		$('.boom-8').removeClass('blink-8');
+		$('.boom-8').addClass('blink-8');
+		console.log('fuck');
+	});
+
+	$('.circle').hover(function(){
+		$('.boom-9').removeClass('blink-9');
+		$('.boom-9').addClass('blink-9');
+		console.log('fuck');
+	});
+
+	$('.circle').hover(function(){
+		$('.boom-10').removeClass('blink-10');
+		$('.boom-10').addClass('blink-10');
+		console.log('fuck');
+	});
+
+	$('.circle').hover(function(){
+		$('.boom-11').removeClass('blink-11');
+		$('.boom-11').addClass('blink-11');
+		console.log('fuck');
+	});
+
+	$('.circle').hover(function(){
+		$('.boom-12').removeClass('blink-12');
+		$('.boom-12').addClass('blink-12');
+		console.log('fuck');
+	});
+
+
 });
