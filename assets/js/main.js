1403a6b6e8184cecfe76e229694b461fe1ddda46
Kelsey Z <kelsey@Kelseys-MacBook-Pro.local>
2017-02-09 13:39:51 -0500

    more booms

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,4 +9,22 @@ $(document).ready(function() {
 		console.log('fuck');
 	});
 
+	$('.circle').hover(function(){
+		$('.boom-2').removeClass('blink-2');
+		$('.boom-2').addClass('blink-2');
+		console.log('fuck');
+	});
+
+	$('.circle').hover(function(){
+		$('.boom-3').removeClass('blink-3');
+		$('.boom-3').addClass('blink-3');
+		console.log('fuck');
+	});
+
+	$('.circle').hover(function(){
+		$('.boom-4').removeClass('blink-4');
+		$('.boom-4').addClass('blink-4');
+		console.log('fuck');
+	});
+
 });
