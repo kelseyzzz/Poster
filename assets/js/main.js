72e4d0d8844d232eb3fa8fce86e456fe457887dc
Kelsey Z <kelsey@Kelseys-MacBook-Pro.local>
2017-02-16 12:46:03 -0500

    hey
    commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,75 +4,143 @@ $(document).ready(function() {
 
 
 	$('.circle').hover(function(){
+		$('.oil').removeClass('move-oil');
+		$('.oil').addClass('move-oil');
+	});
+
+
+	$('.circle').hover(function(){
 		$('.boom').removeClass('blink');
 		$('.boom').addClass('blink');
-		console.log('fuck');
 	});
 
 	$('.circle').hover(function(){
 		$('.boom-2').removeClass('blink-2');
 		$('.boom-2').addClass('blink-2');
-		console.log('fuck');
 	});
 
 	$('.circle').hover(function(){
 		$('.boom-3').removeClass('blink-3');
 		$('.boom-3').addClass('blink-3');
-		console.log('fuck');
 	});
 
 	$('.circle').hover(function(){
 		$('.boom-4').removeClass('blink-4');
 		$('.boom-4').addClass('blink-4');
-		console.log('fuck');
+		
 	});
 
 	$('.circle').hover(function(){
 		$('.boom-5').removeClass('blink-5');
 		$('.boom-5').addClass('blink-5');
-		console.log('fuck');
+		
 	});
 
 	$('.circle').hover(function(){
 		$('.boom-6').removeClass('blink-6');
 		$('.boom-6').addClass('blink-6');
-		console.log('fuck');
+		
 	});
 
 	$('.circle').hover(function(){
 		$('.boom-7').removeClass('blink-7');
 		$('.boom-7').addClass('blink-7');
-		console.log('fuck');
+		
 	});
 
 	$('.circle').hover(function(){
 		$('.boom-8').removeClass('blink-8');
 		$('.boom-8').addClass('blink-8');
-		console.log('fuck');
+		
 	});
 
 	$('.circle').hover(function(){
 		$('.boom-9').removeClass('blink-9');
 		$('.boom-9').addClass('blink-9');
-		console.log('fuck');
+		
 	});
 
 	$('.circle').hover(function(){
 		$('.boom-10').removeClass('blink-10');
 		$('.boom-10').addClass('blink-10');
-		console.log('fuck');
+		
 	});
 
 	$('.circle').hover(function(){
 		$('.boom-11').removeClass('blink-11');
 		$('.boom-11').addClass('blink-11');
-		console.log('fuck');
+		
 	});
 
 	$('.circle').hover(function(){
 		$('.boom-12').removeClass('blink-12');
 		$('.boom-12').addClass('blink-12');
-		console.log('fuck');
+		
+	});
+
+	$('.circle').hover(function(){
+		$('.missle-1').removeClass('move');
+		$('.missle-1').addClass('move');
+	});
+
+	$('.circle').hover(function(){
+		$('.missle-2').removeClass('move-2');
+		$('.missle-2').addClass('move-2');
+	});
+
+	$('.circle').hover(function(){
+		$('.missle-3').removeClass('move-3');
+		$('.missle-3').addClass('move-3');
+	});
+
+	$('.circle').hover(function(){
+		$('.missle-4').removeClass('move-4');
+		$('.missle-4').addClass('move-4');
+	});
+
+	$('.circle').hover(function(){
+		$('.missle-5').removeClass('move-5');
+		$('.missle-5').addClass('move-5');
+	});
+
+	$('.circle').hover(function(){
+		$('.missle-6').removeClass('move-6');
+		$('.missle-6').addClass('move-6');
+	});
+
+	$('.circle').hover(function(){
+		$('.missle-7').removeClass('move-7');
+		$('.missle-7').addClass('move-7');
+	});
+
+	$('.circle').hover(function(){
+		$('.missle-8').removeClass('move-8');
+		$('.missle-8').addClass('move-8');
+	});
+
+	$('.circle').hover(function(){
+		$('.missle-9').removeClass('move-9');
+		$('.missle-9').addClass('move-9');
+	});
+
+	$('.circle').hover(function(){
+		$('.missle-10').removeClass('move-10');
+		$('.missle-10').addClass('move-10');
+	});
+
+	$('.circle').hover(function(){
+		$('.missle-11').removeClass('move-11');
+		$('.missle-11').addClass('move-11');
+	});
+
+	$('.circle').hover(function(){
+		$('.missle-12').removeClass('move-12');
+		$('.missle-12').addClass('move-12');
+	});
+
+	$('.circle').hover(function(){
+		$('.missle-13').removeClass('move-13');
+		$('.missle-13').addClass('move-13');
 	});
 
 
